b06a420c13e820c89f3888f7ae4b387b31956a56
Ravi Silva <ravifelipee@gmail.com>
2023-03-23 00:10:01 -0300

    commit inicial

---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img src="/images/banner.png" alt="O banner principal da página do Organo" />
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div className='campo-texto'>
+            <label>Nome</label>
+            <input placeholder='Digite o seu nome' />
+        </div>
+    )
+
+}
+export default CampoTexto

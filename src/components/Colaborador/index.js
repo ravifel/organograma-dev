e4a0aecb57ac76dea133fa67a97a640521751eb9
Ravi Silva <ravifelipee@gmail.com>
2023-04-02 12:25:39 -0300

    ajustes

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -8,7 +8,7 @@ const Colaborador = (props) => {
                 <img src={props.imagem} alt={props.nome} />
             </div>
             <div className='rodape'>
-                <h4>{props.nome}</h4>
+                <h4 style={{ textTransform: 'uppercase' }}>{props.nome}</h4>
                 <h5>{props.cargo}</h5>
             </div>
         </div>
